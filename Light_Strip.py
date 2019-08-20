e302a46be3b7b78744e2fdc906f9e98099520ea6
James Snyder <J_W_Snyder@yahoo.com>
2019-08-20 08:46:13 -0700

    PEP8 Compliance

--- Light_Strip.py
+++ Light_Strip.py
@@ -19,7 +19,7 @@ def Connect():
 
     for P in Ports:
         strPort = str(P)
-        #CH340 is a generic Arduino from China
+        # CH340 is a generic Arduino from China
         if 'Arduino' in strPort or 'CH340' in strPort:
             splitPort = strPort.split(' ')
             commPort = splitPort[0]
@@ -30,9 +30,10 @@ def Connect():
 
 usb = Connect()
 
+
 def serial_out(C):
     '''
-
+        Pass an ascii encodeded variable throuh the usb serial connection
     '''
     usb.write(C.encode())
 
@@ -42,28 +43,30 @@ window = Tk()
 window.title("Light Strip")
 buttonWidth = 40
 
-# Add an on button and a off button to the window and assign their command functions
-off_switch = Button(window, width = buttonWidth, text = "Off", foreground = "White", background = "Black",
-                    command = lambda: serial_out('0')).grid(row=1)
-White = Button(window, width = buttonWidth, text = "White", background = "White",
-               command = lambda: serial_out('1')).grid( row = 2)
-Red = Button(window, width = buttonWidth, text = "Red", background = "Red",
-             command = lambda: serial_out('2')).grid( row = 3)
-Orange = Button(window, width = buttonWidth, text = "Orange", background = "Orange",
-                command = lambda: serial_out('3')).grid( row=4)
-Yellow = Button(window, width = buttonWidth, text = "Yellow", background = "Yellow",
-                command = lambda: serial_out('4')).grid( row =5)
-Green = Button(window, width = buttonWidth, text = "Green", background = "Green",
-               command =  lambda: serial_out('5')).grid(row =6)
-Blue = Button(window, width = buttonWidth, text = "Blue",
-              background = "Blue", command = lambda: serial_out('6')).grid(row = 7)
-Cyan = Button(window, width = buttonWidth, text = "Cyan",
-              background = "Cyan", command = lambda: serial_out('7')).grid(row = 8)
-Violet = Button(window, width = buttonWidth, text = "Violet",
-                background = "Purple", command = lambda: serial_out('8')).grid(row = 9)
-Rainbow = Button(window, width = buttonWidth, text = "Rainbow",
-                 command = lambda: serial_out('9')).grid(row = 10)
-
+# Add buttons for each color and an off button
+off_switch = Button(window, width=buttonWidth, text="Off",
+                    foreground="White", background="Black",
+                    command=lambda: serial_out('0')).grid(row=1)
+White = Button(window, width=buttonWidth, text="White",
+               background="White",
+               command=lambda: serial_out('1')).grid(row=2)
+Red = Button(window, width=buttonWidth, text="Red", background="Red",
+             command=lambda: serial_out('2')).grid(row=3)
+Orange = Button(window, width=buttonWidth, text="Orange", background="Orange",
+                command=lambda: serial_out('3')).grid(row=4)
+Yellow = Button(window, width=buttonWidth, text="Yellow", background="Yellow",
+                command=lambda: serial_out('4')).grid(row=5)
+Green = Button(window, width=buttonWidth, text="Green", background="Green",
+               command=lambda: serial_out('5')).grid(row=6)
+Blue = Button(window, width=buttonWidth, text="Blue",
+              background="Blue", command=lambda: serial_out('6')).grid(row=7)
+Cyan = Button(window, width=buttonWidth, text="Cyan",
+              background="Cyan", command=lambda: serial_out('7')).grid(row=8)
+Violet = Button(window, width=buttonWidth, text="Violet",
+                background="Purple",
+                command=lambda: serial_out('8')).grid(row=9)
+Rainbow = Button(window, width=buttonWidth, text="Rainbow",
+                 command=lambda: serial_out('9')).grid(row=10)
 
 
 # Start the control loop for the main window
